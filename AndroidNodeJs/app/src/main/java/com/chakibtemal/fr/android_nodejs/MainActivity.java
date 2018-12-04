18b4c7be5c7528ff6c12c409b698ffba10d67a4c
Chakib-Temal <chakibtemal@gmail.com>
2018-12-04 18:40:46 +0100

    v0.2

--- AndroidNodeJs/app/src/main/java/com/chakibtemal/fr/android_nodejs/MainActivity.java
+++ AndroidNodeJs/app/src/main/java/com/chakibtemal/fr/android_nodejs/MainActivity.java
@@ -1,8 +1,12 @@
 package com.chakibtemal.fr.android_nodejs;
 
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.text.TextUtils;
 import android.view.View;
 import android.widget.TextView;
 
@@ -11,10 +15,16 @@ import com.github.nkzawa.socketio.client.IO;
 import com.github.nkzawa.socketio.client.Socket;
 
 import java.net.URISyntaxException;
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class MainActivity extends AppCompatActivity {
 
     private TextView output;
     private Socket mSocket;
+    private SensorManager sensorManager;
+    private Sensor accelero;
+    private float x = 0;
     {
         try {
             mSocket = IO.socket("http://192.168.1.14:9999");
@@ -26,18 +36,26 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mSocket.connect();
-        this.output = (TextView) findViewById(R.id.output);
-
         mSocket.on("hello to you too", onNewMessage);
+
+        this.output = (TextView) findViewById(R.id.output);
+        this.sensorManager = (SensorManager)getSystemService(Context.SENSOR_SERVICE);
+
+        this.accelero = sensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
+
+
+        new Timer().scheduleAtFixedRate(new TimerTask(){
+            @Override
+            public void run(){
+                attemptSend();
+            }
+        },2000,2000);
     }
 
 
     private void attemptSend() {
-        String message = "hello";
-        if (TextUtils.isEmpty(message)) {
-            return;
-        }
-        mSocket.emit("sendInformationUser", message);
+
+        mSocket.emit("sendInformationUser", x);
     }
 
     private Emitter.Listener onNewMessage = new Emitter.Listener() {
@@ -62,5 +80,27 @@ public class MainActivity extends AppCompatActivity {
 
     public void sendMessage(View view) {
         attemptSend();
+    }
+
+    final SensorEventListener mSensorEventListener = new SensorEventListener() {
+        public void onAccuracyChanged(Sensor sensor, int accuracy) {
+        }
+
+        public void onSensorChanged(SensorEvent sensorEvent) {
+            x = sensorEvent.values[0];
+        }
+    };
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        sensorManager.unregisterListener(mSensorEventListener, accelero);
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        sensorManager.registerListener(mSensorEventListener, accelero, SensorManager.SENSOR_DELAY_FASTEST);
     }
 }
